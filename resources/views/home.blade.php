064c6d4bf593d3e3d3c8d4427539d164501aa858
Sam Norton <samnorton@Sams-Mac-mini.local>
2021-10-05 23:04:13 +0800

    Updated registration and login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 <div class="container">
-     <h1>Test</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Hello, {{ auth()->user()->name }}
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
