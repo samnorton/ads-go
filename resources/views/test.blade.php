2c9b1e6d5acce283dc352d26c0d929d31c4ccc3c
Sam Norton <samnorton@Sams-Mac-mini.local>
2021-10-05 21:34:50 +0800

    Installed fortify..

---
+++ resources/views/test.blade.php
@@ -0,0 +1,2 @@
+<link href="{{ asset('css/app.css')}}" rel="stylesheet">
+<button type="button" class="btn btn-primary">Primary</button>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
